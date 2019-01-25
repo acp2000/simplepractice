5fbd710db243fcb2963e2508b3f731f80ff0b709
Adam <adampettyice@gmail.com>
2019-01-25 17:10:38 -0500

    reduces lines of code, less comparisons, uses less memory, decreases runtime

--- funfunc.py
+++ funfunc.py
@@ -12,17 +12,19 @@ def is_float(string):
         return False
 
 miles = ""
-while miles is not "exit":
+while''' miles != ("exit" or "quit")''':
     #made prompt for concise
     miles = input('How many miles would you like to convert to kilometers? ')
     #keep asking for num unless user exits
+#    while not is_float(miles):
+    if miles == "exit" or miles == "quit":
+        break
+
     while not is_float(miles):
-        if miles == "exit":
-            sys.exit()
-        else:
-            print("Please enter a numerical value")
-            miles = input(">")
-    else:
+        print("Please enter a numerical value")
+        miles = input(">")
+
         #made var more descript
-        km = mi_to_km(float(miles))
-        print(f"{miles} miles is {km} kilometers")
+    km = mi_to_km(float(miles))
+    print(f"{miles} miles is {km} kilometers")
+sys.exit()
