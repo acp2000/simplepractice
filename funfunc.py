1c93a10d6f895ec7094da9fb09ce3399a13b425d
tehnyaz <nicholasyarnold@gmail.com>
2019-01-25 16:00:22 -0500

    adds features for reprompt, test for float, exit. makes var/funct names for descript/modular

--- funfunc.py
+++ funfunc.py
@@ -1,14 +1,28 @@
-def convert(miles):
+import sys
+#renamed to make more descript, rm print to make more modular
+def mi_to_km(miles):
     km = 1.60934 * miles
-    print(f"{miles} miles is {km} kilometers")
+    return km
+#returns false if val is not float
+def is_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
 
-print("How many miles would you like to convert to kilometers?")
-
-miles = input(">")
-
-if miles != float(miles):
-    print("Please enter a numerical value")
-    miles = input(">")
-
-float_miles = float(miles)
-convert(float_miles)
+miles = ""
+while miles is not "exit":
+    #made prompt for concise
+    miles = input('How many miles would you like to convert to kilometers? ')
+    #keep asking for num unless user exits
+    while not is_float(miles):
+        if miles == "exit":
+            sys.exit()
+        else:
+            print("Please enter a numerical value")
+            miles = input(">")
+    else:
+        #made var more descript
+        km = mi_to_km(float(miles))
+        print(f"{miles} miles is {km} kilometers")
